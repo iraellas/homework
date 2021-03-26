842086127ea147c06044c15248d093ef4a8dd33a
Irina Detochenko <irina.detochenko@gmail.com>
2021-03-26 21:50:25 +0200

    Solution of Task6

--- src/main/java/Task6.java
+++ src/main/java/Task6.java
@@ -5,7 +5,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Task6 {
     public static void main(String[] args) {
-      
+        System.setProperty("webdriver.chrome.driver", "browserDrivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+
+        String searchFiledXpath = "//input[@title='Поиск']";
+        WebElement searchElement = driver.findElement(By.xpath(searchFiledXpath));
+        searchElement.sendKeys("SoftServe it academy");
+        searchElement.submit();
+        searchElement = driver.findElement(By.className("yuRUbf"));
+        searchElement.click();
+       String currentUrl = driver.getCurrentUrl();
+        driver.quit();
 
     }
 }
